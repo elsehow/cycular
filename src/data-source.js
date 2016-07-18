c7eccac686e05af0fef73487734bbf6694f40938
elsehow <yes@cosmopol.is>
2016-07-17 23:29:14 -0700

    fixed an issue where dataSource.emit and dataSource.error were not available right way

--- src/data-source.js
+++ src/data-source.js
@@ -1,25 +1,25 @@
 var kefir = require('kefir')
+var EventEmitter = require('events').EventEmitter
 
 /*
   TODO document
-  */
+*/
 function source (name, fn) {
-  var newEmitter = {}
-  var S = kefir.stream(emitter => {
-    newEmitter.emit = function (v) {
-      emitter.emit({
-        source: name,
-        timestamp: Date.now(),
-        payload: v,
-      })
-    }
-    newEmitter.error = function (err) {
-      emitter.error(err)
-    }
-  })
-  fn(newEmitter)
+  var emitter = {}
+  var em = new EventEmitter()
+  emitter.emit = function (v) {
+    em.emit('ev', {
+      source: name,
+      timestamp: Date.now(),
+      payload: v,
+    })
+  }
+  emitter.error = function (err) {
+    emitter.error(err)
+  }
+  var S = kefir.fromEvents(em, 'ev')
+  fn(emitter)
   return S
 }
 
 module.exports = source
-
